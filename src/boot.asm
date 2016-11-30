f39d7ece44bad7cec153fcf1ddcb8dfff3454ab2
rmkrupp <beka.krupp@gmail.com>
2016-11-30 10:38:07 -0500

    Day 1

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,42 @@
 ; boot.asm - entry assembly code
 ;
-; Author: Rebecca Krupp (beka.krupp@gmail.com
+; Author: Rebecca Krupp (beka.krupp@gmail.com)
 ;
 
-; TODO
+bits 32
+
+MBALIGN     equ 1
+MEMINFO     equ 2
+FLAGS       equ MBALIGN | MEMINFO
+MAGIC       equ 0x1BADB002
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .bss progbits noalloc noexec write
+align 16
+stack_bottom:
+    times 16384 db 0
+stack_top:
+
+section .text
+global _start
+extern kmain
+_start:
+    mov eax, 0xB8000
+    mov [eax], word 0x044F
+
+    mov esp, stack_top
+    sub esp, 0x8
+    mov [esp], ebx
+    call kmain
+
+    cli
+
+.halt:
+    hlt
+    jmp .halt
