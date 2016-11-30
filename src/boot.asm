bb52c40a958beb71dcefeacad4b57e14b1b50046
rmkrupp <beka.krupp@gmail.com>
2016-11-29 19:24:39 -0500

    prepwork

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,40 @@
 ; boot.asm - entry assembly code
 ;
-; Author: Rebecca Krupp (beka.krupp@gmail.com
+; Author: Rebecca Krupp (beka.krupp@gmail.com)
 ;
 
-; TODO
+bits 32 ; force 32 bit object code
+
+MBALIGN     equ 1
+MEMINFO     equ 2
+FLAGS       equ MBALIGN | MEMINFO
+MAGIC       equ 0x1BADB002
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+VGA_PTR     equ 0xB8000
+
+; multiboot header
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .data
+align 4
+
+section .text
+align 4
+
+global _start ; entrypoint
+
+_start:
+    mov edx, dword VGA_PTR
+    mov ax, 0x0768
+    mov [edx], ax
+
+    cli
+.hang:
+    hlt
+    jmp .hang
+
